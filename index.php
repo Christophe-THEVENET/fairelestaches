3539557c6d98a7f0649ba5774c1cb6cc1c262464
Christophe-HEVENET <christophethevenet2.0@gmail.com>
2022-02-24 16:38:42 +0100

    decoupage du code template / model
    Signed-off-by: Christophe-HEVENET <christophethevenet2.0@gmail.com>

--- index.php
+++ index.php
@@ -1,36 +1,25 @@
-<!DOCTYPE html>
-<html lang="fr">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/png" href="./favicon.png" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <title>fairelestaches</title>
-</head>
-
-<body>
-
-    <?php
-
-    include "./database.php";
-    $pdo = connect();
-
-    foreach ($pdo->query("SELECT * FROM `list` ORDER BY name") as $list) {  ?>
-        <div class="card" style="width: 18rem;">
-            <div class="card-body">
-                <h5 class="card-title" style="color:<?=$list['color']?>"><?= $list['name'] ?></h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-        </div>
-    <?php
-    }
-    ?>
+<?php
 
 
+require_once './templates/header.php';
 
-</body>
+include "./model/database.php";
+$pdo = connect();
 
-</html>
+foreach ($pdo->query("SELECT * FROM `list` ORDER BY name") as $list) {
+
+
+    require './templates/card.php';
+}
+?>
+
+
+<a href="./createList.php" class="btn btn-primary mt-3">Créer une liste</a>
+
+
+<?php
+
+require_once './templates/footer.php';
+
+
+?>
---
+++ model/createList.php
@@ -0,0 +1,14 @@
+<?php
+
+
+require_once './templates/header.php';
+
+
+
+
+
+
+
+
+
+require_once './templates/footer.php';
